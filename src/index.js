e6dea664757085e7fe7734dd41369d11e6efaa73
Kristen <iamkristen220@gmail.com>
2024-10-04 05:32:31 +0100

    meta data optimized

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from "react";
-// import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { hydrate, render } from "react-dom";
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<App />);
-
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
