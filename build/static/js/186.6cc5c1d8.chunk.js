be43006a905da9080c9e1d7764747a12965e30a1
Kristen <iamkristen220@gmail.com>
2024-10-04 05:28:19 +0100

    meta data optimized

---
+++ build/static/js/186.6cc5c1d8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkkristen_portfolio=self.webpackChunkkristen_portfolio||[]).push([[186],{7805:(t,a,r)=>{r.d(a,{I1_:()=>l});var e=r(6688);function l(t){return(0,e.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"}}]})(t)}}}]);
+//# sourceMappingURL=186.6cc5c1d8.chunk.js.map
